f19fa5725e470f2b85fff6f98f96bbe781398bcd
Thai Pangsakulyanont <dtinth@spacet.me>
2021-09-04 10:56:07 +0000

    Image compression

--- screenshot-updater/update.js
+++ screenshot-updater/update.js
@@ -5,54 +5,72 @@ const fs = require('fs')
 const glob = require('glob')
 const mkdirp = require('mkdirp')
 const { basename } = require('path')
+const { ImagePool } = require('@squoosh/lib')
+
 const outputDirectory = process.env.SCREENSHOT_OUTPUT || 'tmp/examples'
 
 async function main() {
   const browser = await chromium.launch()
-  const page = await browser.newPage({
-    deviceScaleFactor: 2,
-  })
-  await page.goto('file://' + __dirname + '/preview.html')
+  const imagePool = new ImagePool()
+  try {
+    const page = await browser.newPage({
+      deviceScaleFactor: 2,
+    })
+    await page.goto('file://' + __dirname + '/preview.html')
 
-  for (const outputFile of glob.sync('tmp/output/*.js')) {
-    const rawData = fs.readFileSync(outputFile, 'utf8')
-    const output = JSON.parse(rawData.slice(rawData.indexOf('{')))
+    for (const outputFile of glob.sync('tmp/output/*.js')) {
+      const rawData = fs.readFileSync(outputFile, 'utf8')
+      const output = JSON.parse(rawData.slice(rawData.indexOf('{')))
 
-    if (output.implicit) {
-      continue
+      if (output.implicit) {
+        continue
+      }
+
+      const name = basename(outputFile, '.js')
+
+      const textFile = `${outputDirectory}/${name}.txt`
+      const pngFile = `${outputDirectory}/${name}.png`
+      const text = output.text
+        .map((line) => line.trimEnd())
+        .join('\n')
+        .trimEnd()
+
+      if (
+        fs.existsSync(textFile) &&
+        fs.readFileSync(textFile, 'utf8') === text
+      ) {
+        console.log(`=> ${name} is up-to-date`)
+        continue
+      }
+
+      const data = output.events
+        .filter((x) => x.type === 'output')
+        .map((x) => x.data)
+        .join('')
+
+      await page.evaluate((data) => {
+        show(data)
+      }, data)
+
+      mkdirp.sync(outputDirectory)
+      const elementHandle = await page.$('#terminal .xterm-screen')
+      const pngBuffer = await elementHandle.screenshot()
+
+      // Compress the image using `@squoosh/lib`
+      const image = imagePool.ingestImage(pngBuffer)
+      await image.preprocess({ quant: { numColors: 64, dither: 0.5 } })
+      await image.encode({ oxipng: {} })
+      const rawEncodedImage = (await image.encodedWith.oxipng).binary
+
+      fs.writeFileSync(textFile, text)
+      fs.writeFileSync(pngFile, rawEncodedImage)
+
+      console.error(`=> ${name} updated`)
     }
-
-    const name = basename(outputFile, '.js')
-
-    const textFile = `${outputDirectory}/${name}.txt`
-    const pngFile = `${outputDirectory}/${name}.png`
-    const text = output.text
-      .map((line) => line.trimEnd())
-      .join('\n')
-      .trimEnd()
-
-    if (fs.existsSync(textFile) && fs.readFileSync(textFile, 'utf8') === text) {
-      console.log(`=> ${name} is up-to-date`)
-      continue
-    }
-
-    const data = output.events
-      .filter((x) => x.type === 'output')
-      .map((x) => x.data)
-      .join('')
-
-    await page.evaluate((data) => {
-      show(data)
-    }, data)
-
-    mkdirp.sync(outputDirectory)
-    const elementHandle = await page.$('#terminal .xterm-screen')
-    await elementHandle.screenshot({ path: pngFile })
-    fs.writeFileSync(textFile, text)
-    console.error(`=> ${name} updated`)
+  } finally {
+    await browser.close()
+    await imagePool.close()
   }
-
-  await browser.close()
 }
 
 main()
