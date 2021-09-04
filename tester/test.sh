d2575d1fed5c0c5d3a194f55a29d8a82cd15a650
Thai Pangsakulyanont <dtinth@spacet.me>
2021-09-04 06:54:40 +0000

    Name it `run-test.js`

--- tester/test.sh
+++ tester/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-docker run --init -ti --rm -v "$(pwd)":/dotfiles -w /dotfiles dotfiles_tester node tester/test.js "$@"
+docker run --init -ti --rm -v "$(pwd)":/dotfiles -w /dotfiles dotfiles_tester node tester/run-test.js "$@"
