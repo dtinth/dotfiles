bf6c340416f137fb0280e78f31377e1b3237cb35
dtinth on MBP M1 <dtinth@spacet.me>
2021-09-21 18:38:08 +0700

    Add automatron script command

---
+++ raycast/scripts/automatron.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+// Required parameters:
+// @raycast.schemaVersion 1
+// @raycast.title automatron
+// @raycast.mode silent
+
+// Optional parameters:
+// @raycast.icon 🤖
+// @raycast.argument1 { "type": "text", "placeholder": "Text to send" }
+// @raycast.packageName dtinth
+
+// Documentation:
+// @raycast.description Invokes the automatron chat bot
+// @raycast.author dtinth
+
+const fs = require('fs')
+const { execSync } = require('child_process')
+const credentialsPath = process.env.HOME + '/.credentials/automatron.json'
+const credentials = JSON.parse(fs.readFileSync(credentialsPath))
+const response = execSync(
+  `curl -s -X POST -H "Content-Type: application/json" -d@- '${credentials.url}/text'`,
+  {
+    input: JSON.stringify({
+      key: credentials.key,
+      text: process.argv[2],
+      source: 'raycast',
+    }),
+    encoding: 'utf8',
+  },
+)
+let text = response
+try {
+  const data = JSON.parse(response)
+  if (data.ok) {
+    const result = data.result
+    if (typeof result === 'string') {
+      text = result
+    } else if (
+      Array.isArray(result) &&
+      result.length === 1 &&
+      result[0].type === 'text'
+    ) {
+      text = result[0].text
+    }
+  }
+} catch (error) {
+  console.error(error)
+}
+console.log(text)
