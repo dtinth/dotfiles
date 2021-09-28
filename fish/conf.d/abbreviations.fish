c98fa55d128a297c98425bde4550d31b835944c2
dtinth on MBP M1 <dtinth@spacet.me>
2021-09-29 02:39:08 +0700

    systemd

--- fish/conf.d/abbreviations.fish
+++ fish/conf.d/abbreviations.fish
@@ -1,1 +1,8 @@
 abbr --add r "npm run"
+
+if command -v systemctl 1>/dev/null 2>&1
+    abbr --add sc "sudo systemctl"
+    abbr --add scu "systemctl --user"
+    abbr --add jc "sudo journalctl -u"
+    abbr --add jcu "journalctl --user"
+end
