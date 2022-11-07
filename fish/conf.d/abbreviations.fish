324cdbf15d53e701780dd50599e7c8b2d38fdd8a
Thai @ Opn <thai@opn.ooo>
2022-11-07 17:16:51 +0000

    Use "docker compose" instead of "docker-compose"

--- fish/conf.d/abbreviations.fish
+++ fish/conf.d/abbreviations.fish
@@ -16,8 +16,8 @@ if command -v systemctl 1>/dev/null 2>&1
     abbr --add jcu "journalctl --user --unit"
 end
 
-if command -v docker-compose 1>/dev/null 2>&1
-    abbr --add dcu "docker-compose up --detach"
-    abbr --add dcx "docker-compose exec"
-    abbr --add dcl "docker-compose logs --tail=100 --follow --timestamps"
+if command -v docker 1>/dev/null 2>&1
+    abbr --add dcu "docker compose up --detach"
+    abbr --add dcx "docker compose exec"
+    abbr --add dcl "docker compose logs --tail=100 --follow --timestamps"
 end
