d340933ee99f9742c9acccb155747c8f392b75c1
dtinth on MBP M1 <dtinth@spacet.me>
2023-04-05 01:56:00 +0700

    sometimes the best commit message is no commit message

--- fish/functions/qc.fish
+++ fish/functions/qc.fish
@@ -1,3 +1,4 @@
 function qc -d "Create a quick commit"
-    git commit -n -m (git diff --stat --staged | tail -n 1)
+    # git commit -n -m (git diff --stat --staged | tail -n 1)
+    git commit -n -m (printf "\u200d")
 end
