508d70433d5a65637ed191317fefe487d7861233
dtinth on MBP M1 <dtinth@spacet.me>
2023-06-08 15:42:54 +0700

    ‌

--- fish/functions/qc.fish
+++ fish/functions/qc.fish
@@ -1,4 +1,4 @@
 function qc -d "Create a quick commit"
     # git commit -n -m (git diff --stat --staged | tail -n 1)
-    git commit -n -m (printf "\u200d")
+    git commit -n -m (printf "\u200c")
 end
