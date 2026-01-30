373c5d74b2612209b6a2691f9083ccc8afbacb65
dtinth-ampere-devbox <dtinth@spacet.me>
2026-01-30 16:35:48 +0700

    Activate mise in all environments including Docker

--- fish/conf.d/mise.fish
+++ fish/conf.d/mise.fish
@@ -1,4 +1,4 @@
-# Only activate mise if it's available and we're not in Docker build
-if command -v mise >/dev/null 2>&1; and test -z "$DOTFILES_BUILDING"; and test ! -f /.dockerenv
+# Activate mise if it's available
+if command -v mise >/dev/null 2>&1
     mise activate fish | source
 end
